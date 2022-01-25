2d8c9bd1f73d86122d29a9e65d23daf6d309b5a9
Valeriya <70814331+luminyanko@users.noreply.github.com>
2022-01-25 19:01:45 +0200

    Update swe-lab.cpp

--- swe-lab/swe-lab.cpp
+++ swe-lab/swe-lab.cpp
@@ -14,34 +14,32 @@ void checkValidParams(int n) {
     }
 }
 
-double y_func(int x, int n) {
+double y_func(double x, int n) {
     double y = 0;
     if (x <= 0) {
         for (int i = 2; i <= n - 1; i++) {
-            cout << (double)x / i << " = " << x << " / " << i << endl;
-            y += (double)x / i;
+            y += x / i;
         }
     }
     else {
         for (int i = 0; i <= n - 1; i++) {
             for (int j = 0; j <= i; j++) {
-                //cout << (double) i / (j + x) << " = " << i << "/ (" << j << "+" << x << ")" << endl;
-                y += (double)i / (j + x);
+                y += i / (j + x);
             }
         }
     }
     return y;
 }
 
-void calculateInRange(int a, int b, int step, int n) {
-    for (int x = a; x <= b; x += step) {
+void calculateInRange(double a, double b, double step, int n) {
+    for (double x = a; x <= b; x += step) {
         cout << "x = " << x << "; y = " << y_func(x, n) << endl;
     }
 }
 
-void checkRange(int& a, int& b) {
+void checkRange(double& a, double& b) {
     if (a > b) {
-        int tmp = a;
+        double tmp = a;
         a = b;
         b = tmp;
     }
@@ -51,7 +49,8 @@ int main()
 {
     try {
 
-        int a, b, n, step;
+        double a, b, step;
+        int n;
 
         cout << "Input a = ";
         cin >> a;
